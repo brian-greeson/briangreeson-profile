6052635e4d0908b3a70c7da3850e7ebdec949a83
brian <3604068+brian-greeson@users.noreply.github.com>
2026-01-15 20:28:00 -0700

    update pages

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ import { devMiddleware } from "./middleware/devMiddleware.js";
 const templates = vento({
   includes: "./src/views/",
 });
-const indexTemplate = await templates.load("pages/index.vto");
-const projectsTemplate = await templates.load("pages/projects.vto");
-const touringTemplate = await templates.load("pages/touring.vto");
-const contactTemplate = await templates.load("pages/contact.vto");
 const app = express();
 app.set("trust proxy", 1);
 
@@ -17,31 +13,36 @@ if (process.env.NODE_ENV !== "production") {
   app.use(devMiddleware(templates));
 }
 
-
-
-
 app.get("/", async (req, res) => {
+  const indexTemplate = await templates.load("pages/index.vto");
+
   const result = await indexTemplate();
   res.type("html").send(result.content);
 });
 
 app.get("/projects", async (req, res) => {
+  const projectsTemplate = await templates.load("pages/projects.vto");
+
   const result = await projectsTemplate();
   res.type("html").send(result.content);
 });
 
 app.get("/touring", async (req, res) => {
+  const touringTemplate = await templates.load("pages/touring.vto");
+
   const result = await touringTemplate();
   res.type("html").send(result.content);
 });
 
 app.get("/contact", async (req, res) => {
+  const contactTemplate = await templates.load("pages/contact.vto");
+
   const result = await contactTemplate();
   res.type("html").send(result.content);
 });
 
 app.get("/up", async (req, res) => {
-  console.log(process.env.DB_FILE_NAME)
+  console.log(process.env.DB_FILE_NAME);
   console.log("up");
   res.sendStatus(200);
 });
