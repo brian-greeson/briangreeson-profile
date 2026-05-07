2682fa8ec07636e993e2234bea16c9ff0961bc40
brian <3604068+brian-greeson@users.noreply.github.com>
2026-05-07 14:38:40 -0600

    fix build script

--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const usersTable = sqliteTable("users_table", {
-  id: int().primaryKey({ autoIncrement: true }),
-  first_name: text().notNull(),
-  last_name: text().notNull(),
-  age: int(),
-  email: text().notNull().unique(),
-});
