9e5c6e7f48f1be1a809ab7574a158bd0bf29e8dd
brian <3604068+brian-greeson@users.noreply.github.com>
2026-05-30 16:00:29 -0600

    add solo tow

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.get("/up", async (req, res) => {
   res.sendStatus(200);
 });
 
-const port = Number(process.env.PORT ?? 3000);
+const port = Number(process.env.PORT ?? 3030);
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
